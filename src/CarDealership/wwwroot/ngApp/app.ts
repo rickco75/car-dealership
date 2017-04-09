e4fd964526668dee88b3d70cf7c6c808c6a3cd70
Rick Cohen <rickco@gmail.com>
2017-04-09 12:30:58 -0400

    cleaned up project

--- src/CarDealership/wwwroot/ngApp/app.ts
+++ src/CarDealership/wwwroot/ngApp/app.ts
@@ -13,12 +13,6 @@ namespace CarDealer {
                 controller: CarDealer.Controllers.HomeController,
                 controllerAs: 'controller'
             })
-            .state('search', {
-                url: '/search',
-                templateUrl: '/ngApp/views/search.html',
-                controller: CarDealer.Controllers.AboutController,
-                controllerAs: 'controller'
-            })
             .state('about', {
                 url: '/about',
                 templateUrl: '/ngApp/views/about.html',
@@ -36,37 +30,4 @@ namespace CarDealer {
         // Enable HTML5 navigation
         $locationProvider.html5Mode(true);
     });
-
-
-
 }
-//namespace CarDealership {
-
-//    angular.module('CarDealership', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
-//        $stateProvider: ng.ui.IStateProvider,
-//        $urlRouterProvider: ng.ui.IUrlRouterProvider,
-//        $locationProvider: ng.ILocationProvider
-//    ) => {
-//        // Define routes
-//        $stateProvider
-//            .state('home', {
-//                url: '/',
-//                templateUrl: '/ngApp/views/home.html',
-//                controller: CarDealership.Controllers.HomeController,
-//                controllerAs: 'controller'
-//            })
-//            .state('notFound', {
-//                url: '/notFound',
-//                templateUrl: '/ngApp/views/notFound.html'
-//            });
-
-//        // Handle request for non-existent route
-//        $urlRouterProvider.otherwise('/notFound');
-
-//        // Enable HTML5 navigation
-//        $locationProvider.html5Mode(true);
-//    });
-
-    
-
-//}
--- src/CarDealership/wwwroot/ngApp/controllers/controllers.ts
+++ src/CarDealership/wwwroot/ngApp/controllers/controllers.ts
@@ -1,9 +1,8 @@
 namespace CarDealer.Controllers {
 
     export class HomeController {
-        //public message = 'Hello from the home page!';
         public cars;
-        public makes; //for later
+        public makes;
         public selectedMake = 0;
         public selectedModel;
 
@@ -39,11 +38,7 @@ namespace CarDealer.Controllers {
 
     angular.module('CarDealer').controller('DialogController', DialogController);
 
-    //one error was bugging me, i did some research on stackoverflow
-    //http://stackoverflow.com/questions/41063947/angular-1-6-0-possibly-unhandled-rejection-error
-    //each time i click outside the modal ui, there were one error message in the console log stating:
-    //'Possibly unhandled rejection: backdrop click
-    //the below piece of code fixes it.
+
     angular.module('CarDealer').config(['$qProvider', function ($qProvider) {
         $qProvider.errorOnUnhandledRejections(false);
     }]);
@@ -54,37 +49,10 @@ namespace CarDealer.Controllers {
         public message = 'Hello from the about page!';
     }
 
-    export class AboutController {
-        //public message = 'Hello from the about page!';
-        public cars;
-        public makes;
-        public search;
-
-
-        fetch() {
-            if (this.search) {
-                this.$http.get(apiUrl + this.search)
-                    .then(res => {
-                        this.cars = res.data;
-                    });
-            }
-        }
-
-        public showModal(carId) {
-            showModalUI(carId, this.$uibModal, this.cars, this.makes)
-        }
-
-        constructor(private makeService: CarDealer.Services.MakeService, private $http: ng.IHttpService, private $uibModal: angular.ui.bootstrap.IModalService) {
-            this.makes = makeService.listMakes();
-        }
-    }
-
     export function showModalUI(carId: number, $uibModal: angular.ui.bootstrap.IModalService, cars, makes) {
 
         let car = cars.find(x => x.id == carId);
         let make = makes.find(x => x.id == car.carMakeId);
-
-        //console.log(make);
 
         $uibModal.open({
             templateUrl: '/ngApp/views/modal.html',
@@ -99,45 +67,3 @@ namespace CarDealer.Controllers {
     }
 
 }
-
-//namespace CarDealership.Controllers {
-
-//    export class HomeController {
-//        public makes;
-//        public cars;
-
-//        //public carSlides: CarSlide[];
-
-//        constructor(private carService: CarDealership.Services.CarService, private $uibModal: ng.ui.bootstrap.IModalService) {
-
-//            this.carService = carService.listCars();
-
-//            this.makes = carService.listMakes();
-//        }
-
-//        public showModal(car) {
-//            this.$uibModal.open(
-//                {
-//                    controller: ModalController,
-//                    controllerAs: 'modal',
-//                    templateUrl: '/ngApp/views/modal.html',
-//                    size: 'md',
-//                    resolve: {
-//                        car: car
-//                    }
-//                });
-//        }
-//    }
-
-//    class ModalController {
-//        constructor(private $uibModalInstance: ng.ui.bootstrap.IModalServiceInstance, public car) { }
-
-//        public ok() {
-//            this.$uibModalInstance.close();
-//        }
-//    }
-
-//    angular.module('CarDealership').controller('homeController', HomeController);
-//    angular.module('CarDealership').controller('modalController', ModalController)
-
-//}
